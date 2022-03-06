587ae58d2ab51c89072b05a7f5e9f6bc86fa87a0
JoeSan <97096980+jookalem@users.noreply.github.com>
2022-03-06 13:27:49 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -11,7 +11,7 @@
 # t.me/SharingUserbot & t.me/Lunatic0de
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP
+from userbot import CMD_HELP, DEVS
 from userbot.utils import skyzu_cmd
 
 GCAST_BLACKLIST = [
