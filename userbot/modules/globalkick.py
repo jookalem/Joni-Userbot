2fc385546643917916809cbec366d6ef08b1c9f9
JoeSan <97096980+jookalem@users.noreply.github.com>
2022-03-06 13:38:45 +0700

    Update globalkick.py

--- userbot/modules/globalkick.py
+++ userbot/modules/globalkick.py
@@ -9,7 +9,8 @@ from telethon.tl.types import MessageEntityMentionName
 
 from userbot import ALIVE_NAME
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP
+from userbot import CMD_HELP, DEVS
+from userbot.events import register
 from userbot.utils import skyzu_cmd
 
 
