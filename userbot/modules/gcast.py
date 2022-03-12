452ffd9e75ea426150dae8e7a681fd8837ebb361
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-12 18:06:50 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -28,6 +28,7 @@ GCAST_BLACKLIST = [
     -1001459701099,  # CatUserbotSupport
     -1001109837870,  # TelegramBotIndonesia
     -1001752592753,  # Skyzusupport
+    -1001687155877,  # CilikSupport
     -1001380293847,  # NastySupport
     -1001664518224,  # JoniSupport
     -1001606097524,  # SanjoSupportt
