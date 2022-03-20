58bd3765c8296370326bc004a19b41efb83864d4
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-03-21 02:53:25 +0700

    Update gid.py

--- userbot/modules/gid.py
+++ userbot/modules/gid.py
@@ -2,10 +2,10 @@ from PIL import Image, ImageDraw, ImageFont
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import skyzu_cmd
+from userbot.utils import joo_cmd
 
 
-@skyzu_cmd(pattern="gid$")
+@joo_cmd(pattern="gid$")
 async def image_maker(event):
     replied_user = await event.get_reply_message()
     await event.client.download_profile_photo(
