186206a6c56ff0b2ec6438b25f3defef9310fd86
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-21 17:06:41 +0700

    Update dbs.py

--- userbot/modules/dbs.py
+++ userbot/modules/dbs.py
@@ -8,14 +8,14 @@
 from platform import uname
 
 from userbot import ALIVE_NAME, BOT_VER, is_mongo_alive, is_redis_alive
-from userbot.events import register
+from userbot.events import joo_cmd
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.db$")
+@joo_cmd(outgoing=True, pattern="^.db$")
 async def amireallydbs(dbs):
     if not is_mongo_alive() and not is_redis_alive():
         db = "Both Mongo and Redis Database seems to be failing!"
@@ -29,6 +29,6 @@ async def amireallydbs(dbs):
         ""
         f"**User :** `{DEFAULTUSER}` \n"
         f"**Status Database:** `{db}`\n"
-        f"**Rose-Userbot :** `{BOT_VER}`"
+        f"**Joni-Userbot :** `{BOT_VER}`"
         ""
     )
