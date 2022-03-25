8a4bef07ecb5843b18250126e1fb558933fafe18
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-25 08:48:05 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -20,7 +20,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@joo_cmd(pattern="help(?: |$)(.*)")
+@joo_cmd(pattern="xhelp(?: |$)(.*)")
 async def help(event):
     """For .help command,"""
     args = event.pattern_match.group(1).lower()
