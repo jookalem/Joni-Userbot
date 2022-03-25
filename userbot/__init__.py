a972a079c7573cbbb2e16bbb1cc0a05e5d1f0b60
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-25 07:42:36 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -833,7 +833,7 @@ with bot:
 
         @tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-                data=re.compile(rb"usage_dyno")
+                data=re.compile(rb"dyno_usage")
             )
         )
         async def on_plug_in_callback_query_handler(event):
